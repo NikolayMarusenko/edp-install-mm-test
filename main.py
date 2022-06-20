5a734a629b73210fb16a21d62b2ac59e8d4908b7
NikolayMarusenko <37263265+NikolayMarusenko@users.noreply.github.com>
2022-06-20 13:44:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ parser = argparse.ArgumentParser(description='Logsight Init')
 parser.add_argument('--username', type=str, help='URL of logsight')
 parser.add_argument('--password', type=str, help='Basic auth username')
 parser.add_argument('--host', type=str, help='Host name')
-parser.add_argument('--baseline_namespace_name', type=str, help='Baseline namespace name')
-parser.add_argument('--baseline_container_name', type=str, help='Baseline container name')
+parser.add_argument('--baseline_namespace', type=str, help='Baseline namespace')
+parser.add_argument('--baseline_container', type=str, help='Baseline container name')
 parser.add_argument('--baseline_container_image', type=str, help='Baseline container image')
-parser.add_argument('--candidate_namespace_name', type=str, help='Candidate namespace name')
-parser.add_argument('--candidate_container_name', type=str, help='Candidate container name')
-parser.add_argument('--candidate_container_image', type=str, help='Candidate container image')
+parser.add_argument('--candidate_namespace', type=str, help='Candidate namespace')
+parser.add_argument('--candidate_container', type=str, help='Candidate container')
+parser.add_argument('--candidate_image', type=str, help='Candidate image')
 parser.add_argument('--risk_threshold', type=int, help='Risk threshold (between 0 and 100)')
 args = parser.parse_args()
 set_host(args.host)
 EMAIL = args.username
 PASSWORD = args.password
 
-BASELINE_TAGS = {"namespace_name": args.baseline_namespace_name, "container_name": args.baseline_container_name, "container_image": args.baseline_container_image}
-CANDIDATE_TAGS = {"namespace_name": args.candidate_namespace_name, "container_name": args.candidate_container_name, "container_image": args.candidate_container_image}
+BASELINE_TAGS = {"namespace": args.baseline_namespace, "container": args.baseline_container, "image": args.baseline_image}
+CANDIDATE_TAGS = {"namespace": args.candidate_namespace, "container": args.candidate_container, "image": args.candidate_image}
 RISK_THRESHOLD = args.risk_threshold
 auth = LogsightAuthentication(email=EMAIL, password=PASSWORD)
 time.sleep(SECONDS_SLEEP)
